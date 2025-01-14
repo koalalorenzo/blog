77b6e4956ee3df0f5c44c1be4358b4fa55d041a3
Lorenzo Setale <lorenzo@setale.me>
2025-01-14 12:18:14 +0100

    adds cli / development tools
    Signed-off-by: Lorenzo Setale <lorenzo@setale.me>

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Lorenzo's Blog";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils }: utils.lib.eachDefaultSystem (msystem:
+    let
+      pkgs = import nixpkgs { system = "${msystem}"; config.allowUnfree = true; };
+    in
+    {
+      devShell = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          coreutils
+          git
+          gnumake
+          gnupg
+          nodePackages.sass
+          hugo
+        ];
+      };
+    }
+  );
+}

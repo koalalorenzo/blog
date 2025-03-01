4ac324d6fe19bbf42da3cbebfb20a4375e4d4889
Lorenzo Setale <lorenzo@setale.me>
2025-03-01 14:58:59 +0100

    switches to nixos stable 24.11

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Lorenzo's Blog";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     utils.url = "github:numtide/flake-utils";
   };
 
